e2ca260c822c303fabc9657cec315ab591c4fe3f
tnaftali <tobias.naftali@redb.ee>
2018-10-24 08:39:39 -0300

    se borran comentarios

--- program.py
+++ program.py
@@ -9,22 +9,6 @@ import threading
 import random
 from logger import Logger
 
-# def manual_input():
-#     max_buffer = input.insert_number('insert maximum buffer capacity: ')
-#     producer_interval = input.insert_number('insert producer seconds interval: ')
-#     consumer_interval = input.insert_number('insert consumer seconds interval: ')
-
-#     buffer = Buffer(max_buffer)
-#     producer = Producer(buffer, producer_interval)    
-#     consumer = Consumer(buffer, consumer_interval)
-
-#     try:
-#         threading.Thread(target=producer.auto_produce).start()
-#         threading.Thread(target=consumer.auto_consume).start()
-#         threading.Thread(target=buffer.auto_print).start()
-#     except:
-#         print ('%s - error: unable to start thread\n' % time.ctime(time.time()))
-
 def main():
     max_buffer = input.insert_number('insert maximum buffer capacity: ')
 
@@ -36,7 +20,6 @@ def main():
     try:
         threading.Thread(target=producer.auto_produce).start()
         threading.Thread(target=consumer.auto_consume).start()
-        # threading.Thread(target=buffer.auto_print).start()
     except:
         print(Logger.error('Unable to start thread'))
 
